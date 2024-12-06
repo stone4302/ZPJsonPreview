e443b026ba5ae3872c3cc3d480626352dbd7efb6
pengbingxiang <pengbingxiang@weli.cn>
2024-12-06 16:24:13 +0800

    first commit

--- pod_repo_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-echo "enter dir: `pwd`"
-
-pod repo push 58corp-zpspecs ZPJsonPreview.podspec --allow-warnings --sources="git@igit.58corp.com:HRG-Client/iOS/ZPSpecs.git" --use-libraries
